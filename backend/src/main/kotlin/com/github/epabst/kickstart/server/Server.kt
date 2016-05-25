cf6375f00ff82ae9999f54ea206bcb9ab58764dc
Eric Pabst <epabst@gmail.com>
2016-05-24 20:19:48 -0600

    Add skeleton for multi-module project.
    Used JustACluelessNewbie's answer to
      https://stackoverflow.com/questions/34999133/shared-code-between-kotlin-server-and-client
    for the build.gradle files.

---
+++ backend/src/main/kotlin/com/github/epabst/kickstart/server/Server.kt
@@ -0,0 +1,10 @@
+package com.github.epabst.kickstart.server
+
+/**
+ * The RESTful Server.
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 5/11/16
+ * Time: 3:51 PM
+ */
+object Server {
+}
---
+++ webclient/src/main/kotlin/main.kt
@@ -0,0 +1,36 @@
+import net.yested.el
+import net.yested.Div
+import net.yested.with
+
+/**
+ * The main entrypoint of the app.
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 5/17/16
+ * Time: 6:25 AM
+ */
+fun main(args: Array<String>) {
+
+    //here we create some div with a header and a list
+    val div = Div() with {
+        h1 {
+            +"Hello World" //plus add given text as textContent to HTML element (h1)
+        }
+        ul {
+            li {
+                strong {
+                    +"Bolded text"
+                }
+            }
+            li {
+                a(href = "http://www.yested.net") {
+                    +"Link to Framework"
+                }
+            }
+        }
+    }
+
+    //when we have constructed a DOM, we can take a parent element (via div.element)
+    //and append it as a child to "app" div in HTML page
+    el("page")!!.appendChild(div.element)
+
+}
