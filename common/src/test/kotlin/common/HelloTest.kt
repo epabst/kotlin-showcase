09c2b7c182d0f553983b54be615846a5c1bd62f4
Eric Pabst <epabst@gmail.com>
2016-06-22 12:44:59 -0600

    Switch to Spek for JVM tests.

--- common/src/test/kotlin/common/HelloTest.kt
+++ common/src/test/kotlin/common/HelloTest.kt
@@ -1,6 +1,6 @@
 package common
 
-import org.junit.Test
+import org.jetbrains.spek.api.Spek
 import kotlin.test.assertEquals
 
 /**
@@ -9,14 +9,12 @@ import kotlin.test.assertEquals
  * Date: 6/21/16
  * Time: 11:28 PM
  */
-class HelloTest {
-    @Test
-    fun hello() {
+class HelloTest : Spek({
+    it("should say hello using a name") {
         assertEquals("Hello Bob", Hello.hello("Bob"))
     }
 
-    @Test
-    fun helloWorld() {
+    it("should say hello to the world by default") {
         assertEquals("Hello World", Hello.hello())
     }
-}
+})
---
+++ common/src/test/kotlin/common/spec.kt
@@ -0,0 +1,13 @@
+package common
+
+import kotlin.test.assertEquals
+
+/**
+ * Kotlin extensions to QUnit.
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 6/16/16
+ * Time: 5:59 AM
+ */
+fun <T> T.mustBe(expected: T) {
+    assertEquals(this, expected)
+}
