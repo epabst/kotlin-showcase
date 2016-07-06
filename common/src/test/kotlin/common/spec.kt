fb9950bca1b2ec1e56ba94b9fbf2dbd5161240b2
Eric Pabst <epabst@gmail.com>
2016-07-05 23:48:00 -0600

    Add mustNotBe to spec.kt.

--- common/src/test/kotlin/common/spec.kt
+++ common/src/test/kotlin/common/spec.kt
@@ -1,6 +1,10 @@
 package common
 
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.fail
 
 /**
  * Kotlin extensions to QUnit.
@@ -10,4 +14,21 @@ import kotlin.test.assertEquals
  */
 fun <T> T.mustBe(expected: T) {
     assertEquals(expected, this)
+}
+
+fun <T> T.mustNotBe(expected: T) {
+    assertNotEquals(expected, this)
+}
+
+fun String?.mustContain(expectedSubstring: String) {
+    MatcherAssert.assertThat(this, CoreMatchers.containsString(expectedSubstring))
+}
+
+fun <E : Exception> intercept(block: () -> Unit): E {
+    try {
+        block()
+        fail("expected exception")
+    } catch (exception: E) {
+        return exception
+    }
 }
