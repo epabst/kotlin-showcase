8ab73ca4c4b40c036a6c09d9f44dd4d75edefb00
Eric Pabst <epabst@gmail.com>
2017-11-18 00:13:05 -0700

    Add handlingErrors.

--- webclient/src/main/kotlin/client/util/jsMonitoring.kt
+++ webclient/src/main/kotlin/client/util/jsMonitoring.kt
@@ -1,7 +1,18 @@
 package client.util
 
+import common.util.inContext
 import kotlin.browser.window
 
 fun handleError(throwable: Throwable) {
     window.alert(common.util.currentContext + ": " + throwable.toString())
+}
+
+fun handlingErrors(contextName: String, f: ()->Any) {
+    return inContext(contextName) {
+        try {
+            f()
+        } catch (e: Throwable) {
+            handleError(e)
+        }
+    }
 }
