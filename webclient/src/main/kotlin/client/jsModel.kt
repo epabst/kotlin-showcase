fd5bd9b75ab3ac8c5cb046aac2e4276af8a92c64
Eric Pabst <epabst@gmail.com>
2018-01-05 22:40:22 -0700

    Dedup code in jsModel.kt.

--- webclient/src/main/kotlin/client/jsModel.kt
+++ webclient/src/main/kotlin/client/jsModel.kt
@@ -4,10 +4,6 @@ import client.util.*
 import client.util.IDJS
 import client.util.RichDateJS
 import common.*
-import common.util.ID
-import common.util.RichDate
-import common.util.WithID
-import net.yested.ext.moment.Moment
 
 /**
  * JSON support for the core model classes.
@@ -15,42 +11,6 @@ import net.yested.ext.moment.Moment
  * Date: 6/9/16
  * Time: 6:27 AM
  */
-external interface LongJS {
-    val high_: Int
-    val low_: Int
-}
-
-private val HIGH_AMOUNT: Long = (Int.MAX_VALUE.toLong() + 1) * 2
-
-fun LongJS?.toNormal(): Long? {
-    if (this == null) {
-        return null
-    }
-    else if (high_ == undefined) {
-        val number: Number = asDynamic()
-        return number.toLong()
-    }
-    else {
-        return high_.toLong() * HIGH_AMOUNT + (if (low_ >= 0) low_.toLong() else low_.toLong() + HIGH_AMOUNT)
-    }
-}
-
-fun <E : WithID<E>> String.toID(): ID<E>? = JSON.parse<LongJS>(this).toNormal()?.let { ID(it) }
-
-fun RichDate.toMoment(): Moment = (date as MomentDate).moment
-
-external interface RichDateJS {
-    val months: Int
-    val days: Int
-}
-
-fun RichDateJS.toNormal(): RichDate = RichDate(months, days)
-
-external interface IDJS {
-    val id: LongJS
-}
-
-fun <E : WithID<E>> IDJS.toNormal(): ID<E>? = id.toNormal()?.let { ID(it) }
 
 external interface ToDoJS {
     val name: String
