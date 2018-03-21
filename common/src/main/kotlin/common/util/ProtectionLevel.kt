9fe090933e74c2618423e1580295e0e49509d3c6
Eric Pabst <epabst@gmail.com>
2018-03-21 05:23:13 -0600

    Use ProtectionLevel in all entities for app.

--- common/src/main/kotlin/common/util/ProtectionLevel.kt
+++ common/src/main/kotlin/common/util/ProtectionLevel.kt
@@ -6,20 +6,18 @@ package common.util
  * Date: 3/19/18
  * Time: 6:47 AM
  */
-enum class ProtectionLevel {
+enum class ProtectionLevel(val label: String) {
+    /** Owned by all users. */
+    PUBLIC("Public: modifiable by all users"),
+
+    /** Private to the owning user.  It is visible an all of their devices. */
+    PRIVATE("Private: only visible to me, but on any device"),
+
     /**
      * Only stored on the particular device.
      * It is not stored in a cloud and is not accessible by anyone unless they have access to the device.
      */
-    DEVICE,
-
-    /** Private to the owning user.  It is visible an all of their devices. */
-    PRIVATE,
-
-    /**
-     * Owned by all users.
-     */
-    PUBLIC
+    DEVICE("Device: Kept only on this device")
 }
 
 interface ProtectedWithID<T : ProtectedWithID<T>> : WithID<T> {
