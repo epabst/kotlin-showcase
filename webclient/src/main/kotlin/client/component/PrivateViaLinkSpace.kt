b780495ac97c577440b708ceb91f9c4de193b887
Eric Pabst <epabst@gmail.com>
2018-04-24 17:42:55 -0600

    Clean up code for PrivateViaLinkSpace.

--- webclient/src/main/kotlin/client/component/PrivateViaLinkSpace.kt
+++ webclient/src/main/kotlin/client/component/PrivateViaLinkSpace.kt
@@ -42,6 +42,16 @@ fun PrivateViaLinkSpaceRepository(userId: ReadOnlyProperty<String?>, firebaseApp
     return PrivateFirebaseRepository<PrivateViaLinkSpace,PrivateViaLinkSpaceJS>(userId, "privateViaLinkSpaceList", { it.toNormal() }, firebaseApp)
 }
 
+fun Repository<PrivateViaLinkSpace>.populateSpaceIdAndCopyLinkToClipboard(accessProperty: Property<Access>, spaceName: ReadOnlyProperty<String>, destinationHash: String) {
+    var access = accessProperty.get()
+    if (access.protectionLevel == ProtectionLevel.PRIVATE_VIA_LINK) {
+        access = withNewSpaceIfNeeded(access, spaceName.get())
+        accessProperty.set(access)
+        val currentLocationHash = window.location.hash
+        copyTextToClipboard(window.location.href.replace(currentLocationHash, PrivateViaLinkSpaceModel.toUrl(access.privateViaLinkSpaceId!!, destinationHash)))
+    }
+}
+
 fun Repository<PrivateViaLinkSpace>.withNewSpaceIfNeeded(access: Access, spaceName: String): Access {
     return if (access.protectionLevel != ProtectionLevel.PRIVATE_VIA_LINK || access.privateViaLinkSpaceId != null) {
         access
@@ -55,6 +65,16 @@ fun Repository<PrivateViaLinkSpace>.addIfMissing(privateViaLinkSpaceId: ID<Priva
     if (!privateViaLinkSpaceIds.contains(privateViaLinkSpaceId)) {
         save(PrivateViaLinkSpace("Shared " + PlatformProvider.instance.now().toDisplayDateTimeString(), privateViaLinkSpaceId))
     }
+}
+
+private fun copyTextToClipboard(text: String) {
+    val element = document.createElement("textarea") as HTMLTextAreaElement
+    val body = document.body!!
+    body.appendChild(element)
+    element.value = text
+    element.select()
+    document.execCommand("copy")
+    body.removeChild(element)
 }
 
 class PrivateViaLinkSpaceModel(firebaseApp: App) {
@@ -78,7 +98,8 @@ class PrivateViaLinkSpaceModel(firebaseApp: App) {
 fun BtsFormContext.accessControl(access: Property<Access>,
                                  selectableAccessList: ReadOnlyProperty<List<Access>>,
                                  spaceName: ReadOnlyProperty<String>,
-                                 destinationHash: ReadOnlyProperty<String>): HTMLDivElement {
+                                 destinationHash: ReadOnlyProperty<String>,
+                                 privateViaLinkSpaceRepository: Repository<PrivateViaLinkSpace>): HTMLDivElement {
     return btsFormItemSimple(state = Property(State.Default), label = "Access Level") {
         val singleSelectInput = singleSelectInput(access, selectableAccessList) {
             textContent = it.protectionLevel.label
@@ -86,31 +107,15 @@ fun BtsFormContext.accessControl(access: Property<Access>,
         selectableAccessList.onNext { singleSelectInput.disabled = it.size <= 1 }
         val copied = access.mapAsDefault { false }
         btsButton(size = ButtonSize.Default, look = ButtonLook.Info,
-                onclick = { it.stopImmediatePropagation(); populateSpaceIdAndCopyLinkToClipboard(access, spaceName, destinationHash.get()); copied.set(true) }) {
+                onclick = {
+                    it.stopImmediatePropagation()
+                    privateViaLinkSpaceRepository.populateSpaceIdAndCopyLinkToClipboard(access, spaceName, destinationHash.get())
+                    copied.set(true)
+                }) {
             access.onNext { visible = it.protectionLevel == ProtectionLevel.PRIVATE_VIA_LINK }
             appendText("Copy Link")
         }
         nbsp()
         span { copied.onNext { textContent = if (it) "Copied." else "" } }
     }
-}
-
-fun populateSpaceIdAndCopyLinkToClipboard(accessProperty: Property<Access>, spaceName: ReadOnlyProperty<String>, destinationHash: String) {
-    var access = accessProperty.get()
-    if (access.protectionLevel == ProtectionLevel.PRIVATE_VIA_LINK) {
-        access = Factory.privateViaLinkSpaceRepository.withNewSpaceIfNeeded(access, spaceName.get())
-        accessProperty.set(access)
-        val currentLocationHash = window.location.hash
-        copyTextToClipboard(window.location.href.replace(currentLocationHash, PrivateViaLinkSpaceModel.toUrl(access.privateViaLinkSpaceId!!, destinationHash)))
-    }
-}
-
-private fun copyTextToClipboard(text: String) {
-    val element = document.createElement("textarea") as HTMLTextAreaElement
-    val body = document.body!!
-    body.appendChild(element)
-    element.value = text
-    element.select()
-    document.execCommand("copy")
-    body.removeChild(element)
 }
