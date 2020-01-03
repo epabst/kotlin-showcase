44605ee474659916db01cc9c6e4f565edfccb5ac
Eric Pabst <epabst@gmail.com>
2020-01-03 03:29:36 -0700

    Use Firestore's FirestoreDataConverter

--- src/jsMain/kotlin/component/firebase/FirestoreRepositorySync.kt
+++ src/jsMain/kotlin/component/firebase/FirestoreRepositorySync.kt
@@ -4,33 +4,43 @@ import component.repository.*
 import firebase.firestore.*
 import kotlinx.coroutines.await
 
-fun <T : WithID<T>> CollectionReference<DocumentData>.doc(id: ID<T>?): DocumentReference<DocumentData> {
+fun <T : WithID<T>> CollectionReference<T>.doc(id: ID<T>?): DocumentReference<T> {
     val documentPath = id?.toString()
     return if (documentPath == null) doc() else doc(documentPath)
 }
 
-suspend fun <E : WithID<E>> DocumentReference<DocumentData>.set(entity: E) {
+suspend fun <E : WithID<E>> DocumentReference<E>.set(entity: E) {
     set(JSON.parse(JSON.stringify(entity.withID(ID(id))))).await()
 }
 
-interface JsonParser<E : WithID<E>> {
-    fun fromJson(json: DocumentData): E
+suspend fun <E : WithID<E>> CollectionReference<E>.save(entity: E) {
+    doc(entity.getID()).set(entity).await()
 }
 
-fun <E : WithID<E>> DocumentSnapshot<DocumentData>.parse(parser: JsonParser<E>): E {
-    return parser.fromJson(data()!!).withID(ID(id))
+fun <E : WithID<E>> QuerySnapshot<E>.toList(): List<E> {
+    return docs.map { snapshot -> snapshot.data() }
 }
 
-fun <E : WithID<E>> QuerySnapshot<DocumentData>.parse(parser: JsonParser<E>): List<E> {
-    return this.docs.map { snapshot -> parser.fromJson(snapshot.data()).withID(ID(snapshot.id)) }
-}
-
-fun <E : WithID<E>> Query<DocumentData>.addListener(parser: JsonParser<E>, block: (List<E>) -> Unit): Closeable {
+fun <E : WithID<E>> Query<E>.addListener(block: (List<E>) -> Unit): Closeable {
     val release = onSnapshot(onNext = { snapshot ->
         console.log("got a Firestore snapshot size=${snapshot.size}")
-        block.invoke(snapshot.docs.map { parser.fromJson(it.data()) })
+        block.invoke(snapshot.docs.map { it.data() })
     })
     return object : Closeable {
         override fun close() = release.invoke()
     }
+}
+
+interface FirestoreEntityConverter<E : WithID<E>> : FirestoreDataConverter<E> {
+
+    fun toNormal(json: DocumentData): E
+
+    override fun toFirestore(modelObject: E): DocumentData {
+        return JSON.parse(JSON.stringify(modelObject))
+    }
+
+    override fun fromFirestore(snapshot: QueryDocumentSnapshot<DocumentData>, options: SnapshotOptions): E {
+        return toNormal(snapshot.data()).withID(ID(snapshot.id))
+    }
+
 }
