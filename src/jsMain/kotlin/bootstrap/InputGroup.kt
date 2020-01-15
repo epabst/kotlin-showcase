f5c7b345eba84949c264995df4e69574429a447f
Eric Pabst <epabst@gmail.com>
2020-01-14 17:42:08 -0700

    Fix some react-bootstrap external definitions with correct properties

--- src/jsMain/kotlin/bootstrap/InputGroup.kt
+++ src/jsMain/kotlin/bootstrap/InputGroup.kt
@@ -1,23 +1,26 @@
-@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION", "unused")
+@file:Suppress(
+    "INTERFACE_WITH_SUPERCLASS",
+    "OVERRIDING_FINAL_MEMBER",
+    "RETURN_TYPE_MISMATCH_ON_OVERRIDE",
+    "CONFLICTING_OVERLOADS",
+    "EXTERNAL_DELEGATION",
+    "unused"
+)
 @file:JsModule("react-bootstrap")
+
 package bootstrap
 
+import org.w3c.dom.HTMLDivElement
 import react.RProps
 
-abstract external class InputGroupAppend<As : React.ElementType> : BsPrefixComponent<As, RProps>
-abstract external class InputGroupPrepend<As : React.ElementType> : BsPrefixComponent<As, RProps>
-abstract external class InputGroupText<As : React.ElementType> : BsPrefixComponent<As, RProps>
-abstract external class InputGroupCheckbox : BsPrefixComponent<React.String /* 'input' */, RProps>
-abstract external class InputGroupRadio : BsPrefixComponent<React.String /* 'input' */, RProps>
-external interface InputGroupProps : RProps {
+external interface InputGroupProps : HTMLElementProps<HTMLDivElement> {
     var size: String /* 'sm' | 'lg' */
 }
+
 abstract external class InputGroup<As : React.ElementType> : BsPrefixComponent<As, InputGroupProps> {
-    companion object {
-        var Append: Any
-        var Prepend: Any
-        var Text: Any
-        var Checkbox: Any
-        var Radio: Any
-    }
+    abstract class Append<As : React.ElementType> : BsPrefixComponent<As, RProps>
+    abstract class Prepend<As : React.ElementType> : BsPrefixComponent<As, RProps>
+    abstract class Text<As : React.ElementType> : BsPrefixComponent<As, RProps>
+    abstract class Checkbox : BsPrefixComponent<React.String /* 'input' */, RProps>
+    abstract class Radio : BsPrefixComponent<React.String /* 'input' */, RProps>
 }
