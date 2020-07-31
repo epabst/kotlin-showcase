4738f3c77b627fe3e4fbd5734bd3d594e1065f43
Eric Pabst <epabst@gmail.com>
2020-07-30 23:43:20 -0600

    Add enzyme wrappers to be convenient from Kotlin

---
+++ src/jsTest/kotlin/enzyme/enzyme_wrappers.kt
@@ -0,0 +1,90 @@
+@file:Suppress("unused")
+package enzyme
+
+import kotlinext.js.jsObject
+import react.*
+import react.ReactElement
+import kotlin.reflect.KClass
+
+private val configureEnzyme = configure(jsObject { adapter = Adapter() })
+
+fun <P : RProps, S : RState, C : Component<P, S>> shallow(
+    klazz: KClass<out C>
+): ShallowWrapper<P, S, C> {
+    return shallow(toElement(klazz))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> shallow(
+    klazz: KClass<out C>,
+    handler: RHandler<P>
+): ShallowWrapper<P, S, C> {
+    return shallow(toElement(klazz, handler))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> shallow(
+    klazz: KClass<out C>,
+    props: P
+): ShallowWrapper<P, S, C> {
+    return shallow(toElement(klazz, props))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> shallow(
+    klazz: KClass<out C>,
+    props: P,
+    handler: RHandler<P>
+): ShallowWrapper<P, S, C> {
+    return shallow(toElement(klazz, props, handler))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> mount(
+    klazz: KClass<out C>
+): ReactWrapper<P, S, C> {
+    return mount(toElement(klazz))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> mount(
+    klazz: KClass<out C>,
+    handler: RHandler<P>
+): ReactWrapper<P, S, C> {
+    return mount(toElement(klazz, handler))
+}
+
+fun <P : RProps, S : RState, C : Component<P, S>> mount(
+    klazz: KClass<out C>,
+    props: P,
+    handler: RHandler<P>
+): ReactWrapper<P, S, C> {
+    return mount(toElement(klazz, props, handler))
+}
+
+private fun <P : RProps, S : RState, C : Component<P, S>> toElement(
+    klazz: KClass<out C>
+): ReactElement {
+    return toElement(klazz, handler = {})
+}
+
+private fun <P : RProps, S : RState, C : Component<P, S>> toElement(
+    klazz: KClass<out C>,
+    handler: RHandler<P>
+): ReactElement {
+    return toElement(klazz, jsObject(), handler)
+}
+
+private fun <P : RProps, S : RState, C : Component<P, S>> toElement(
+    klazz: KClass<out C>,
+    props: P
+): ReactElement {
+    return toElement(klazz, props) {}
+}
+
+private fun <P : RProps, S : RState, C : Component<P, S>> toElement(
+    klazz: KClass<out C>,
+    props: P,
+    handler: RHandler<P>
+): ReactElement {
+    val children = with(RElementBuilder(props)) {
+        handler()
+        childList
+    }
+    return createElement(klazz.rClass, props, *children.toTypedArray())
+}
